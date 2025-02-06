e200f20c3d28e4a926a7265f2313738f4f71338c
Manoj <manojs@Manojs-MacBook-Air.local>
2025-02-06 12:36:05 +0530

    add

--- app/componets/dashboard.tsx
+++ app/componets/dashboard.tsx
@@ -4,39 +4,32 @@ import { ReactElement, useState } from "react";
 
 const Dashboard = (): ReactElement => {
   const { instance } = useMsal();
-  const [loading, setLoading] = useState(false);
+  const [loadingSignIn, setLoadingSignIn] = useState(false);
+  const [loadingSignUp, setLoadingSignUp] = useState(false);
 
   const handleSignIn = async () => {
-    setLoading(true);
-    await instance
-      .loginRedirect(signInRequest)
-      .then(() => {
-        console.log("Logged In Successfully");
-      })
-      .catch((e) => {
-        console.error(e);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    setLoadingSignIn(true);
+    try {
+      await instance.loginRedirect(signInRequest);
+      console.log("Logged In Successfully");
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoadingSignIn(false);
+    }
   };
 
   const handleSignUp = async () => {
-    setLoading(true);
-    await instance
-      .loginRedirect(signUpRequest)
-      .then(() => {
-        console.log("Successfully Logged Out");
-      })
-      .catch((e) => {
-        console.error(e);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    setLoadingSignUp(true);
+    try {
+      await instance.loginRedirect(signUpRequest);
+      console.log("Successfully Signed Up");
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoadingSignUp(false);
+    }
   };
-
-  console.log(loading,'loading')
 
   return (
     <div className="flex items-center justify-center h-full w-[800px]">
@@ -45,37 +38,33 @@ const Dashboard = (): ReactElement => {
           Welcome to Century Cricket App
         </h1>
 
-        {loading ? (
-          <div className="flex items-center justify-center">
-            <div className="loader">laoding</div>
+        <div className="space-y-6">
+          <div>
+            <h2 className="text-xl font-semibold mb-2 text-gray-700">
+              Have an account?
+            </h2>
+            <button
+              className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              onClick={handleSignIn}
+              disabled={loadingSignIn}
+            >
+              {loadingSignIn ? "Signing In..." : "Sign In"}
+            </button>
           </div>
-        ) : (
-          <div className="space-y-6">
-            <div>
-              <h2 className="text-xl font-semibold mb-2 text-gray-700">
-                Have an account?
-              </h2>
-              <button
-                className="w-full bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                onClick={handleSignIn}
-              >
-                Sign In
-              </button>
-            </div>
 
-            <div className="border-t border-gray-300 pt-6">
-              <h2 className="text-xl font-semibold mb-2 text-gray-700">
-                New here?
-              </h2>
-              <button
-                className="w-full bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
-                onClick={handleSignUp}
-              >
-                Create Account
-              </button>
-            </div>
+          <div className="border-t border-gray-300 pt-6">
+            <h2 className="text-xl font-semibold mb-2 text-gray-700">
+              New here?
+            </h2>
+            <button
+              className="w-full bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+              onClick={handleSignUp}
+              disabled={loadingSignUp}
+            >
+              {loadingSignUp ? "Creating Account..." : "Create Account"}
+            </button>
           </div>
-        )}
+        </div>
 
         <p className="mt-8 text-center text-sm text-gray-600">
           By signing in or creating an account, you agree to our Terms of
